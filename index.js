cf6ba519be83f3f53a701560856a6a38ac706dfd
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-01-06 17:55:08 +0900

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const navbar = document.getElementsByClassName("navbar");
+const weather = document.getElementsByClassName("weather-section");
+const account = document.getElementsByClassName("account-section");
+const accountButton = document.getElementsByClassName("account-button");
+
+const main = document.querySelector("main");
+
+const todo = document.getElementsByClassName("todo");
+const todoForm = document.getElementsByClassName("todo-form");
+const todoInput = document.getElementsByClassName("todo-input");
+const todoButton = document.getElementsByClassName("todo-button");
+const todoListContainer = document.getElementsByClassName(
+  "todo-list-container"
+);
+const todoList = document.getElementsByClassName("todo-list");
+
+const footer = document.querySelector("footer");
+const time = document.getElementsByClassName("clock");
